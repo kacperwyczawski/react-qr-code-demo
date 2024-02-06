5524e0a6184d2e19fe2d177cb72265611afe08da
Kacper Wyczawski <kacper@wyczawski.dev>
2024-02-06 16:49:01 +0100

    feat: implement basic QR code scanning

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import './App.css'
+import QrReader from 'react-qr-scanner'
+
+export default function App() {
+  const employees = [
+    { id: 1, firstName: 'John', lastName: 'Doe', age: 25 },
+    { id: 2, firstName: 'Anna', lastName: 'Smith', age: 35 },
+    { id: 3, firstName: 'Peter', lastName: 'Jones', age: 45 },
+    { id: 4, firstName: 'Katy', lastName: 'Johnson', age: 55 }
+  ]
+  const [data, setData] = useState('No result')
+  const selectedEmployee = employees.find((employee) => employee.id === Number(data))
+
+  return (
+    <>
+      {data === 'No result' ? (
+        <QrReader
+          onScan={(capturedData) => {
+            setData(capturedData?.text ?? 'No result')
+            console.info(capturedData?.text ? `Captured id ${capturedData.text}` : 'No result')
+          }}
+          onError={(err) => {
+            console.error(err)
+          }}
+          delay={1000}
+        />
+      ) : (selectedEmployee ? (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{selectedEmployee.id}</td>
+              <td>{selectedEmployee.firstName}</td>
+              <td>{selectedEmployee.lastName}</td>
+              <td>{selectedEmployee.age}</td>
+            </tr>
+          </tbody>
+        </table>
+      ) : (
+        <p>
+          Employee with id {data} does not exist
+        </p>
+      ))}
+      
+      <aside>
+        Available employees: [ {employees.map((employee) => employee.id).join(', ')} ]
+      </aside>
+    </>
+  )
+}
